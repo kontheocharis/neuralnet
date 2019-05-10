164366b0ef1397c7ed7aa3003cbf85eb970aadbc
Constantine Theocharis <kontheocharis@gmail.com>
2019-05-10 20:53:20 +0200

    Add image generation for doc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from PIL import Image
 
 tf.reset_default_graph()
 
@@ -10,18 +13,29 @@ fashion_mnist = keras.datasets.fashion_mnist
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+# IMAGE GENERATION
+# indices = [3, 40, 600, 800]
+# class_names = ['Tshirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Boot']
+
+# for i in indices:
+#     image = train_images[i]
+#     image = np.array(image, dtype='float')
+#     pixels = image.reshape((28, 28))
+#     im = Image.fromarray(pixels).convert("L")
+#     im.save("images/" + str(class_names[train_labels[i]]) + str(i) + ".jpeg")
+
 train_labels = tf.one_hot(train_labels, 10)
 test_labels = tf.one_hot(test_labels, 10)
 
 model = keras.Sequential([
     keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),
-    keras.layers.Conv2D(8, (3,3), activation=tf.nn.relu),
+    keras.layers.Conv2D(8, (3, 3), activation=tf.nn.relu),
     keras.layers.Flatten(input_shape=()),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-model.compile(optimizer='adam', 
+model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
